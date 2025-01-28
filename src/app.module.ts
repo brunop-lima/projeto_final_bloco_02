4ba9f6b91009a2764075fef72dd0af0e921a0ddf
CHEKE01 <134391765+CHEKE01@users.noreply.github.com>
2025-01-28 15:13:35 -0300

    :sparkle: security

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { Produto } from "./produto/entities/produto.entity";
 import { ProdutoModule } from "./produto/produto.module";
 import { Categoria } from "./categoria/entities/categoria.entity";
 import { CategoriaModule } from "./categoria/categoria.module";
+import { AuthModule } from "./auth/auth.module";
+import { Usuario } from "./usuario/entities/usuario.entity";
+import { UsuarioModule } from "./usuario/usuario.module";
 
 @Module({
   imports:[
@@ -14,11 +17,13 @@ import { CategoriaModule } from "./categoria/categoria.module";
       username:'root',
       password:'root',
       database: 'db_projeto02',
-      entities: [Produto, Categoria],
+      entities: [Produto, Categoria, Usuario],
       synchronize: true,
     }),
     ProdutoModule,
     CategoriaModule,
+    AuthModule,
+    UsuarioModule,
   ],
   controllers:[],
   providers:[],
---
+++ src/usuario/services/usuario.service.ts
@@ -0,0 +1,86 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from '../entities/usuario.entity';
+import { Bcrypt } from '../../auth/bcrypt/bcrypt';
+import { differenceInYears } from 'date-fns';
+
+
+@Injectable()
+export class UsuarioService {
+    constructor(
+        @InjectRepository(Usuario)
+        private usuarioRepository: Repository<Usuario>,
+    ) {}
+
+    
+    async findByUsuario(usuario: string): Promise<Usuario |   null > {
+        return  await this.usuarioRepository.findOne({
+            where: {
+                usuario: usuario
+            }
+        })
+    }
+
+
+    async findAll(): Promise<Usuario[]> {
+        return await this.usuarioRepository.find({  
+            relations: {  
+              produto: true  }  
+          }) 
+
+    }
+
+    async findById(id: number): Promise<Usuario> {
+
+        const usuario = await this.usuarioRepository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                produto: true
+            }
+        });
+        if (!usuario)
+            throw new HttpException('Usuario não encontrado!', HttpStatus.NOT_FOUND);
+
+        return usuario;
+
+    }
+
+    async create(usuario: Usuario): Promise<Usuario> {
+
+        const dataNascimento = usuario.data_nasc;  
+        const [dia, mes, ano] = dataNascimento.split('-').map(Number);  
+        const dataNascimentoDate = new Date(ano, mes - 1, dia);  
+        const dataAtual = new Date();  
+        const idade = differenceInYears(dataAtual, dataNascimentoDate);  
+    
+        if (idade < 18)  
+          throw new HttpException('⚠️ Usuário deve ter pelo menos 18 anos', HttpStatus.NOT_FOUND); 
+        
+        const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+        if (buscaUsuario)
+            throw new HttpException("⚠️ O Usuario já existe!", HttpStatus.BAD_REQUEST);
+
+        return await this.usuarioRepository.save(usuario);
+
+    }
+
+    async update(usuario: Usuario): Promise<Usuario> {
+
+        await this.findById(usuario.id);
+
+        const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+        if (buscaUsuario && buscaUsuario.id !== usuario.id)
+            throw new HttpException('Usuário já Cadastrado!', HttpStatus.BAD_REQUEST);
+
+        await this.create(usuario)
+
+        return await this.usuarioRepository.save(usuario);
+
+    }
+
+}
